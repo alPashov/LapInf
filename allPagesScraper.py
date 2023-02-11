8b7d0ae2de6fe2c5fbe1245c7402a9f6fd3defd3
al-pashov <al-pashov>
2023-02-11 22:44:19 +0200

    Defined get_html_list function

--- allPagesScraper.py
+++ allPagesScraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 import csv
 import pandas as pd
 
-list_uls = [
+list_urls = [
     #Gaming Links 
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/kapacitet-pamet-f7886,nad-16-gb-v25917/tip-video-pamet-f7895,dopylnitelna-v-2754/tip-syhranenie-f7941,ssd-v-4670679/price,between-1000-and-3000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
     #Everyday/Home 
@@ -12,3 +12,13 @@ list_uls = [
     "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/tip-video-pamet-f7895,vgradena-v-2755/avtonomija-baterija-f7901,nad-6-h-v25862/tip-syhranenie-f7941,ssd-v-4670679/kapacitet-ssd-f9203,500-gb-v-4694038/kapacitet-ssd-f9203,1-tb-v-4690219/kapacitet-ssd-f9203,512-gb-v-4685751/price,between-979-and-5732/rating,star-4/c"
 ]
 
+def get_html_list():
+    htmls = []
+    with sync_playwright() as pl:
+        browser = pl.firefox.launch(headless=False)
+        page = browser.new_page()
+        for url in list_urls:
+            page.goto(url)
+            page.wait_for_selector("div#card_grid")
+            htmls.append(page.inner_html("div#card_grid"))
+    return htmls
