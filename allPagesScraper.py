b267d13200ad392be9777097d6e705a672121ae1
al-pashov <al-pashov>
2023-02-11 22:40:06 +0200

    Defined Lists

--- allPagesScraper.py
+++ allPagesScraper.py
@@ -3,3 +3,12 @@ from bs4 import BeautifulSoup as bs
 import csv
 import pandas as pd
 
+list_uls = [
+    #Gaming Links 
+    "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/kapacitet-pamet-f7886,nad-16-gb-v25917/tip-video-pamet-f7895,dopylnitelna-v-2754/tip-syhranenie-f7941,ssd-v-4670679/price,between-1000-and-3000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
+    #Everyday/Home 
+    "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,6-8-gb-v25915/tip-video-pamet-f7895,vgradena-v-2755/price,between-500-and-1000/rating,star-4/c?ref=lst_leftbar_6420_4-5",
+    #Office 
+    "https://www.emag.bg/laptopi/filter/kapacitet-pamet-f7886,12-16-gb-v25916/tip-video-pamet-f7895,vgradena-v-2755/avtonomija-baterija-f7901,nad-6-h-v25862/tip-syhranenie-f7941,ssd-v-4670679/kapacitet-ssd-f9203,500-gb-v-4694038/kapacitet-ssd-f9203,1-tb-v-4690219/kapacitet-ssd-f9203,512-gb-v-4685751/price,between-979-and-5732/rating,star-4/c"
+]
+
